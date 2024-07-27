0db5bb727123b195006abb57a178689e93fc0726
startho <154859404+startho@users.noreply.github.com>
2024-07-27 16:16:19 +0900

    yuyu

--- Frontend/src/components/Carousel3.js
+++ Frontend/src/components/Carousel3.js
@@ -1,100 +1,111 @@
 import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import "../css/Carousels3.css";
 import Modal from "react-bootstrap/Modal";
+import "../css/Carousels3.css";
 
 const Carousel3 = () => {
     const [index, setIndex] = useState(0);
     const [modalShow, setModalShow] = useState(false);
-    const [modalImage, setModalImage] = useState("");
-    
-  
-    const handleSelect = (selectedIndex) => {
-      setIndex(selectedIndex);
-    };
-  
-    const handleImageClick = (imageSrc) => {
-      setModalImage(imageSrc);
-      setModalShow(true);
-    };
-    
-    
-  
-    return (
+    const [modalIndex, setModalIndex] = useState(0);
+    const [modalImages, setModalImages] = useState([]);
 
-  <>
-  <Carousel activeIndex={index} onSelect={handleSelect} className="custom-carousel2">
-    <Carousel.Item>
-      <div className="image-container2">
-        <img
-          className="custom-img2"
-          src="/img/mig.png"
-          alt="First slide"
-          onClick={() => handleImageClick("/img/mig.png")}
-        />
-        <img
-          className="custom-img2"
-          src="/img/dmer.jpg"
-          alt="Second slide"
-          onClick={() => handleImageClick("/img/dmer.jpg")}
-        />
-        <img
-          className="custom-img2"
-          src="/img/main3.png"
-          alt="Third slide"
-          onClick={() => handleImageClick("/img/main3.png")}
-        />
-        <img
-          className="custom-img2"
-          src="/img/mig.png"
-          alt="First slide"
-          onClick={() => handleImageClick("/img/mig.png")}
-        />
-       
-      </div>
-    </Carousel.Item>  
-    <Carousel.Item>
-      <div className="image-container2">
-        <img
-          className="custom-img2"
-          src="/img/mig.png"
-          alt="First slide"
-          onClick={() => handleImageClick("/img/mig.png")}
-        />
-        <img
-          className="custom-img2"
-          src="/img/dmer.jpg"
-          alt="Second slide"
-          onClick={() => handleImageClick("/img/dmer.jpg")}
-        />
-        <img
-          className="custom-img2"
-          src="/img/main3.png"
-          alt="Third slide"
-          onClick={() => handleImageClick("/img/main3.png")}
-        />
-        <img
-          className="custom-img2"
-          src="/img/mig.png"
-          alt="First slide"
-          onClick={() => handleImageClick("/img/mig.png")}
-        />
-        
-      </div>
-    </Carousel.Item>
-    {/* 추가 슬라이드가 필요하면 위와 동일하게 추가 */}
-  </Carousel>
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+
+    const handleImageClick = (selectedIndex, imagesArray) => {
+        setModalImages(imagesArray);
+        setModalIndex(selectedIndex);
+        setModalShow(true);
+    };
+
+    const handleModalSelect = (selectedIndex) => {
+        setModalIndex(selectedIndex);
+    };
   
-  <Modal show={modalShow} onHide={() => setModalShow(false)} centered>
-    <Modal.Header closeButton>
-      {/* <Modal.Title>Image Preview</Modal.Title> */}
-    </Modal.Header>
-    <Modal.Body>
-      <img src={modalImage} alt="Modal Preview" className="modal-img" />
-    </Modal.Body>
-  </Modal>
-  </>
-  );
-  }
+    const images1 = [
+        "/img/mig.png",
+        "/img/dmer.jpg",
+        "/img/main3.png",
+        "/img/mig.png",
+        "/img/mig.png",
+        "/img/dmer.jpg",
+        "/img/main3.png",
+        "/img/mig.png"
+    ];
+    
+    const images2 = [
+        "/img/mig.png",
+        "/img/dmer.jpg",
+        "/img/main3.png",
+        "/img/mig.png",
+        "/img/mig.png",
+        "/img/dmer.jpg",
+        "/img/main3.png",
+        "/img/mig.png"
+    ];
+    
+    return (
+        <>
+            <div className="carousel-section">
+                <h2 className="carousel-title">Carousel 1</h2>
+                <Carousel activeIndex={index} onSelect={handleSelect} className="custom-carousel2">
+                    {images1.map((image, idx) => (
+                        <Carousel.Item key={idx}>
+                            <div className="image-container2">
+                                {images1.map((image, idx) => (
+                                    <img
+                                        key={idx}
+                                        className="custom-img2"
+                                        src={image}
+                                        alt={`Slide ${idx + 1}`}
+                                        onClick={() => handleImageClick(idx, images1)}
+                                    />
+                                ))}
+                            </div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </div>
+
+            <div className="carousel-section">
+                <h2 className="carousel-title">Carousel 2</h2>
+                <Carousel activeIndex={index} onSelect={handleSelect} className="custom-carousel2">
+                    {images2.map((image, idx) => (
+                        <Carousel.Item key={idx}>
+                            <div className="image-container2">
+                                {images2.map((image, idx) => (
+                                    <img
+                                        key={idx}
+                                        className="custom-img2"
+                                        src={image}
+                                        alt={`Slide ${idx + 1}`}
+                                        onClick={() => handleImageClick(idx, images2)}
+                                    />
+                                ))}
+                            </div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </div>
+      
+            <Modal show={modalShow} onHide={() => setModalShow(false)} className="modal-fullscreen" centered>
+                <Modal.Body className="modal-body-fullscreen">
+                    <Carousel activeIndex={modalIndex} onSelect={handleModalSelect} className="w-100 h-100">
+                        {modalImages.map((image, idx) => (
+                            <Carousel.Item key={idx}>
+                                <img
+                                    className="carousel-img-fullscreen"
+                                    src={image}
+                                    alt={`Modal Slide ${idx + 1}`}
+                                />
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
 
 export default Carousel3;

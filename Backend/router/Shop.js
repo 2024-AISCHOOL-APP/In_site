26602726f0fa99fd64a98f175791e1ae3ab8ee25
startho <154859404+startho@users.noreply.github.com>
2024-07-30 12:50:18 +0900

    yy

--- Backend/router/Shop.js
+++ Backend/router/Shop.js
@@ -12,7 +12,6 @@ router.get("/:store_idx", (req, res) => {
     conn.query(sql, [store_idx], (err, rows) => {
       if (err) {
         console.error("샵 인포 라우터 쿼리 오류:", err);
-        s;
         res.status(500).json({ error: "서버 오류" });
         return;
       }
@@ -53,6 +52,31 @@ router.get("/:store_idx", (req, res) => {
     });
   });
   
+// 제품 정보 조회 라우터
+router.get("/p/:store_idx", (req, res) =>{
+  let store_idx = req.params.store_idx; // URL에서 mem_id를 가져옴
+  console.log("샵 인포 라우터 store_idx:",store_idx);
+
+  let sql = "SELECT * FROM tbl_product WHERE store_idx = ?"; // mem_id를 이용한 SQL 쿼리
+
+  conn.query(sql, [store_idx], (err, rows) => {
+    if (err) {
+      console.error("샵 인포 라우터 쿼리 오류:", err);
+      res.status(500).json({ error: "서버 오류" });
+      return;
+    }
+    if (rows.length > 0) {
+      console.log("데이터dd 가져와짐");
+      res.json({
+        productInfo: rows,
+      });
+    } else {
+      console.log("내정보 라우터 값 문제");
+      res.status(404).json({ error: "데이터가 없습니다." });
+    }
+  });
+});
+
 
 
 

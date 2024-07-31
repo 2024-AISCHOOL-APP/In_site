1d564c979256efae4f388609478c59bb63b5460a
startho <154859404+startho@users.noreply.github.com>
2024-07-31 11:00:13 +0900

    yy

--- Frontend/src/pages/Shop.js
+++ Frontend/src/pages/Shop.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Image, Form, Button } from "react-bootstrap";
-import {useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "../axios";
 import Map from "../components/Map";
-// import Carousels from "../components/Carousel";
-// import Carousels2 from "../components/Carousel2";
 import Carousels3 from "../components/Carousel3";
+import "../css/shop.css";
+
+const formatPrice = (price) => {
+  return new Intl.NumberFormat('ko-KR').format(price);
+};
 
 const Shop = () => {
   const { store_idx } = useParams();
@@ -13,6 +16,7 @@ const Shop = () => {
   const [productInfo, setProductInfo] = useState([]);
   const [selectedHall, setSelectedHall] = useState('');
   const [selectedInfo, setSelectedInfo] = useState({ guestCount: '', price: '' });
+  const [selectedHallImage, setSelectedHallImage] = useState('');
 
   useEffect(() => {
     const fetchStoreDetail = async () => {
@@ -28,7 +32,7 @@ const Shop = () => {
       try {
         const response = await axios.get(`/shop/p/${store_idx}`);
         setProductInfo(response.data.productInfo);
-        console.log(response.data.productInfo,"tfghfgh");
+        console.log(response.data.productInfo, "tfghfgh");
       } catch (error) {
         console.error("제품 정보를 가져오는 도중 오류 발생:", error);
       }
@@ -37,38 +41,51 @@ const Shop = () => {
     fetchStoreDetail();
   }, [store_idx]);
 
-
-
   const handleSelectChange = (event) => {
     const selectedHall = event.target.value;
     setSelectedHall(selectedHall);
 
     const selected = productInfo.find(info => info.prod_name === selectedHall);
+
     if (selected) {
-      setSelectedInfo({ guestCount: selected.prod_info, price: selected.prod_price === 0 ? null : selected.prod_price });
+        console.log(selected.prod_img, "Selected product image");
+        setSelectedInfo({ guestCount: selected.prod_info, price: selected.prod_price === 0 ? null : selected.prod_price });
+        setSelectedHallImage(selected.prod_img || ''); // 선택된 홀의 이미지 URL 업데이트
     } else {
-      setSelectedInfo({ guestCount: '', price: null });
+        setSelectedInfo({ guestCount: '', price: null });
+        setSelectedHallImage(storeDetail.store_img); // 선택 해제 시 이미지 초기화
     }
-  };
+};
+
+  console.log(selectedHallImage.prod_img);
+
+ 
 
   return (
     <>
-    <Row className="mt-5"></Row>
-    <Container>
-      <Row className="mt-5">
-        <Col lg={12} md={12} sm={12} className="t2">
-          {storeDetail.store_name}
-        </Col>
-      </Row>
+      <Row className="mt-5"></Row>
+      <Container>
+        <Row className="mt-5">
+          <Col lg={12} md={12} sm={12} className="t2">
+            {storeDetail.store_name}
+          </Col>
+        </Row>
 
-      <Row className="my-5">
-        <Col lg={6} md={6} sm={12}>
-          <Image src={storeDetail.store_img} />
-        </Col>
-        <Col lg={6} md={6} sm={12} className="t2 login-form-container">
-          <Row className="mb-3 mt-4">
-            <Col>
-            <Form.Select onChange={handleSelectChange}>
+        <Row className="my-5">
+          <Col lg={6} md={6} sm={12}>
+          <Image className="img-fluid2"
+              src={selectedHallImage || storeDetail.store_img} 
+              alt="Selected Hall" 
+              fluid 
+              onError={(e) => {
+                e.target.src = storeDetail.store_img; // 이미지 로드 실패 시 기본 이미지로 변경
+              }}
+            />
+          </Col>
+          <Col lg={6} md={6} sm={12} className="t2 login-form-container">
+            <Row className="mb-3 mt-4">
+              <Col>
+                <Form.Select onChange={handleSelectChange}>
                   <option value="">홀을 선택하세요</option>
                   {productInfo.length > 0 ? (
                     productInfo.map((info, index) => (
@@ -80,11 +97,11 @@ const Shop = () => {
                     <option>제품 정보를 불러오는 중...</option>
                   )}
                 </Form.Select>
-            </Col>
-          </Row>
-          {selectedHall && (
-            <>
-              {selectedInfo.guestCount && (
+              </Col>
+            </Row>
+            {selectedHall && (
+              <>
+                {selectedInfo.guestCount && (
                   <Row className="mb-3">
                     <Col>
                       <p className="smoneys">하객수 : {selectedInfo.guestCount}</p>
@@ -94,57 +111,54 @@ const Shop = () => {
                 {selectedInfo.price !== null && selectedInfo.price !== '' && (
                   <Row className="mb-3">
                     <Col>
-                      <p className="smoneys">가격 : {selectedInfo.price}</p>
+                      <p className="smoneys">가격 : {formatPrice(selectedInfo.price)}</p>
                     </Col>
                   </Row>
                 )}
-            </>
-          )}
-          <Row className="mb-3 align-items-center">
-            <Col className="d-flex align-items-center">
-              <span className="sdates mb-0 pe-2">일정 :</span>
-              <Form.Control
-                type="date"
-                className="mb-0"
-                style={{ display: "inline-block", width: "50%" }}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button variant="success" type="submit" className="login-button mt-5">
-                장바구니
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+              </>
+            )}
+            {/* <Row className="mb-3 align-items-center">
+              <Col className="d-flex align-items-center">
+                <span className="sdates mb-0 pe-2">일정 :</span>
+                <Form.Control
+                  type="date"
+                  className="mb-0"
+                  style={{ display: "inline-block", width: "50%" }}
+                />
+              </Col>
+            </Row> */}
+            <Row>
+              <Col>
+                <Button variant="success" type="submit" className="login-button mt-5">
+                  장바구니
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
 
-      <Row className="my-5">
-        <Col lg={12} md={12} sm={12} className="t2">
-          정보
-        </Col>
-      </Row>
-      <Row className="my-5 d-flex justify-content-center">
-        <Col>
-          <Carousels3 />
-        </Col>
-      </Row>
+        <Row className="my-5">
+          <Col lg={12} md={12} sm={12} className="t2"></Col>
+        </Row>
+        <Row className="my-5 d-flex justify-content-center">
+          <Col>
+            <Carousels3 store_idx={store_idx} />
+          </Col>
+        </Row>
 
-      <Row>
-        <Col lg={6} md={12} sm={12} className="t2">
-          오시는 길
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Map lat={storeDetail.lat} lon={storeDetail.lon} />
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <Col lg={6} md={12} sm={12} className="t2">
+            오시는 길
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Map lat={storeDetail.lat} lon={storeDetail.lon} />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
-
 
 export default Shop;
